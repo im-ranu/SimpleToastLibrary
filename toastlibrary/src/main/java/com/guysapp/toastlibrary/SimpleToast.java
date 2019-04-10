3bf34b507af05a14bf00b3b246a95721f3047224
Ranjeet Chouhan <ranjeet@callhippo.com>
2019-04-10 15:21:39 +0530

    Toast Library Added

---
+++ toastlibrary/src/main/java/com/guysapp/toastlibrary/SimpleToast.java
@@ -0,0 +1,13 @@
+package com.guysapp.toastlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class SimpleToast {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
